40e897ed1b0a37556ab76106c385a2956c253fe7
honaz140 <honaz140@gmail.com>
2023-04-19 21:45:37 +0700

    fix deploy error

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,12 +4,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+// import { deleteTodo } from '../../businessLogic/todos'
+// import { getUserId } from '../utils'
 
 export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
+  async (_: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+   // const todoId = event.pathParameters.todoId
     // TODO: Remove a TODO item by id
     
     return undefined

82af5cb8cf3657cd20bc3bba4676e65dd75a8537
honaz140 <honaz140@gmail.com>
2023-04-22 11:05:31 +0700

    redeploy

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'o277nr8rs9'
+const apiId = 's3c9jodw26'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
